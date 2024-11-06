fb1113893c22094da4c3a6c147c5d994f64bfb37
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-11-06 22:36:48 +0530

    Day 5 add transaction function

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,7 +1,11 @@
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
-
+import {
+  useWallet,
+  InputTransactionData,
+} from "@aptos-labs/wallet-adapter-react";
+import { AccountAddress, Aptos, AptosConfig } from "@aptos-labs/ts-sdk";
+import { AptosClient, Network } from "aptos";
 
 const GameWrapper1 = () => {
   return (
@@ -16,7 +20,7 @@ const GameWrapper1 = () => {
   );
 };
 
-const GameWrapper2 = ()=>{
+const GameWrapper2 = () => {
   return (
     <div>
       {/* <div>
@@ -24,54 +28,121 @@ const GameWrapper2 = ()=>{
       </div> */}
       <div className="h-screen flex justify-center align-middle">
         <div className="my-auto w-4/5">
-        <div className="flex justify-center">
-          <button className="bg-green-500 mx-auto px-6 py-2 rounded-xl text-white my-2">Start Game</button>
-        </div>
-        <div className=" p-5">
-          <div className="flex gap-2">
-            {/* Card Component */}
-            <div className="bg-white w-1/2 rounded-2xl p-5">
-              <div>
-              <div className="bg-gray-300 mx-auto px-6 py-4 rounded-xl text-black font-semibold text-xl text-center my-2">Select Your Move</div>
+          <div className="flex justify-center">
+            <button className="bg-green-500 mx-auto px-6 py-2 rounded-xl text-white my-2">
+              Start Game
+            </button>
+          </div>
+          <div className=" p-5">
+            <div className="flex gap-2">
+              {/* Card Component */}
+              <div className="bg-white w-1/2 rounded-2xl p-5">
+                <div>
+                  <div className="bg-gray-300 mx-auto px-6 py-4 rounded-xl text-black font-semibold text-xl text-center my-2">
+                    Select Your Move
+                  </div>
+                </div>
+                <div className="flex gap-2">
+                  <button className="bg-red-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                    Clear
+                  </button>
+                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                    Rock
+                  </button>
+                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                    Paper
+                  </button>
+                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                    Scissors
+                  </button>
+                </div>
               </div>
-              <div className="flex gap-2">
-                <button className="bg-red-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">Clear</button>
-                <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">Rock</button>
-                <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">Paper</button>
-                <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">Scissors</button>
-              </div>
-            </div>
-            <div className="bg-white w-1/2 rounded-2xl p-5">
-              <div>
-              <div className="bg-gray-300 mx-auto px-6 py-4 rounded-xl text-black font-semibold text-xl text-center my-2">Computer Move</div>
-              </div>
-              <div className="flex gap-2">
-                <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">Rock</button>
-                <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">Paper</button>
-                <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">Scissors</button>
+              <div className="bg-white w-1/2 rounded-2xl p-5">
+                <div>
+                  <div className="bg-gray-300 mx-auto px-6 py-4 rounded-xl text-black font-semibold text-xl text-center my-2">
+                    Computer Move
+                  </div>
+                </div>
+                <div className="flex gap-2">
+                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                    Rock
+                  </button>
+                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                    Paper
+                  </button>
+                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                    Scissors
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="">
-          <div className="bg-green-500 w-3/5 mx-auto px-6 py-4 rounded-xl text-black font-semibold text-4xl text-center my-2">Game Results</div>
-        </div>
+          <div className="">
+            <div className="bg-green-500 w-3/5 mx-auto px-6 py-4 rounded-xl text-black font-semibold text-4xl text-center my-2">
+              Game Results
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 function App() {
   const { account, connected, signAndSubmitTransaction } = useWallet();
+  const moduleAddress = "";
+  const moduleName = "";
+  const client = new Aptos(new AptosConfig({ network: Network.TESTNET }));
+
+  const toggleActiveState = async () => {
+    if (!account) return;
+    const payload: InputTransactionData = {
+      data: {
+        function: `${moduleAddress}::${moduleName}::createGame`,
+        functionArguments: [],
+      },
+    };
+    const response = await signAndSubmitTransaction(payload);
+    console.log(response);
+  };
+
+  const handleOperationClick = async (operation: string) => {
+    if (
+      operation === "Rock" ||
+      operation === "Paper" ||
+      operation === "Scissors"
+    ) {
+      try {
+        if (!account) return;
+
+        const payload: InputTransactionData = {
+          data: {
+            function: `${moduleAddress}::${moduleName}::duel`,
+            functionArguments: [operation],
+          },
+        };
+        const response = await signAndSubmitTransaction(payload);
+        console.log(response);
+        const resultData = await client.getAccountResource({
+          accountAddress: account?.address,
+          resourceType: `${moduleAddress}::${moduleName}::DuelResult`,
+        });
+        console.log(resultData);
+      } catch (error) {
+        console.error(error);
+      } finally {
+      }
+    }
+  };
+
   return (
     <>
       <div className="w-screen h-screen flex flex-col justify-center align-middle bg-neutral-100">
         <div className="absolute right-4 top-4 items-end">
-            <WalletSelector />
+          <WalletSelector />
         </div>
         {connected ? GameWrapper2() : GameWrapper1()}
-    </div>
+      </div>
     </>
   );
 }
