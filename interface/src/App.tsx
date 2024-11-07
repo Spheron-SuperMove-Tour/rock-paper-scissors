d5b7815acd2e0a728223e1445606b1eace1349e9
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-11-07 22:09:39 +0530

    Day 6 integrating with frontend

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -6,6 +6,12 @@ import {
 } from "@aptos-labs/wallet-adapter-react";
 import { AccountAddress, Aptos, AptosConfig } from "@aptos-labs/ts-sdk";
 import { AptosClient, Network } from "aptos";
+import { useState } from "react";
+
+const moduleAddress =
+  "0x610ea90387f24c61fa507060dfb272a901ef420411473ab344cc45d72904e3bb";
+const moduleName = "RockPaperScissors_01";
+const client = new Aptos(new AptosConfig({ network: Network.TESTNET }));
 
 const GameWrapper1 = () => {
   return (
@@ -20,7 +26,21 @@ const GameWrapper1 = () => {
   );
 };
 
-const GameWrapper2 = () => {
+const GameWrapper2 = ({
+  gameState,
+  onToggleGame,
+  onMoveSelection,
+  result,
+  computerSelection,
+  transactionInProgress,
+}: {
+  gameState: boolean;
+  onToggleGame: () => void;
+  onMoveSelection: (move: string) => void;
+  result: string;
+  computerSelection: string;
+  transactionInProgress: boolean;
+}) => {
   return (
     <div>
       {/* <div>
@@ -29,8 +49,11 @@ const GameWrapper2 = () => {
       <div className="h-screen flex justify-center align-middle">
         <div className="my-auto w-4/5">
           <div className="flex justify-center">
-            <button className="bg-green-500 mx-auto px-6 py-2 rounded-xl text-white my-2">
-              Start Game
+            <button
+              className="bg-green-500 mx-auto px-6 py-2 rounded-xl text-white my-2"
+              onClick={onToggleGame}
+            >
+              {!gameState ? "Start Game" : "Stop Game"}
             </button>
           </div>
           <div className=" p-5">
@@ -43,18 +66,22 @@ const GameWrapper2 = () => {
                   </div>
                 </div>
                 <div className="flex gap-2">
-                  <button className="bg-red-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
+                  <button
+                    className="bg-red-300 mx-auto px-8 py-4 text-xl rounded-xl my-2"
+                    onClick={() => onMoveSelection("Clear")}
+                  >
                     Clear
                   </button>
-                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
-                    Rock
-                  </button>
-                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
-                    Paper
-                  </button>
-                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
-                    Scissors
-                  </button>
+                  {["Rock", "Paper", "Scissors"].map((move) => (
+                    <button
+                      key={move}
+                      className="bg-red-300 mx-auto px-8 py-4 text-xl rounded-xl my-2"
+                      onClick={() => onMoveSelection(move)}
+                      disabled={transactionInProgress || !gameState}
+                    >
+                      {move}
+                    </button>
+                  ))}
                 </div>
               </div>
               <div className="bg-white w-1/2 rounded-2xl p-5">
@@ -64,22 +91,23 @@ const GameWrapper2 = () => {
                   </div>
                 </div>
                 <div className="flex gap-2">
-                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
-                    Rock
-                  </button>
-                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
-                    Paper
-                  </button>
-                  <button className="bg-orange-300 mx-auto px-8 py-4 text-xl rounded-xl my-2">
-                    Scissors
-                  </button>
+                  {["Rock", "Paper", "Scissors"].map((move) => (
+                    <button
+                      key={move}
+                      className="bg-red-300 mx-auto px-8 py-4 text-xl rounded-xl my-2"
+                      onClick={() => onMoveSelection(move)}
+                      disabled={transactionInProgress || !gameState}
+                    >
+                      {move === computerSelection ? move : ""}
+                    </button>
+                  ))}
                 </div>
               </div>
             </div>
           </div>
           <div className="">
             <div className="bg-green-500 w-3/5 mx-auto px-6 py-4 rounded-xl text-black font-semibold text-4xl text-center my-2">
-              Game Results
+              {result || "Game Result"}
             </div>
           </div>
         </div>
@@ -90,48 +118,66 @@ const GameWrapper2 = () => {
 
 function App() {
   const { account, connected, signAndSubmitTransaction } = useWallet();
-  const moduleAddress = "";
-  const moduleName = "";
-  const client = new Aptos(new AptosConfig({ network: Network.TESTNET }));
+  const [gameState, setGameState] = useState(false);
+  const [result, setResult] = useState("");
+  const [computerSelection, setComputerSelection] = useState("");
+  const [transactionInProgress, setTransactionInProgress] = useState(false);
 
-  const toggleActiveState = async () => {
+  const toggleGameState = async () => {
     if (!account) return;
+    setGameState(!gameState);
+
     const payload: InputTransactionData = {
       data: {
         function: `${moduleAddress}::${moduleName}::createGame`,
         functionArguments: [],
       },
     };
-    const response = await signAndSubmitTransaction(payload);
-    console.log(response);
+    await handleTransaction(payload);
+    setResult("");
+    setComputerSelection("");
+  };
+  const handleMoveSelection = async (move: string) => {
+    if (move === "Clear") {
+      setComputerSelection("");
+      setResult("");
+      return;
+    }
+    const payload: InputTransactionData = {
+      data: {
+        function: `${moduleAddress}::${moduleName}::duel`,
+        functionArguments: [move],
+      },
+    };
+    await handleTransaction(payload, move);
   };
 
-  const handleOperationClick = async (operation: string) => {
-    if (
-      operation === "Rock" ||
-      operation === "Paper" ||
-      operation === "Scissors"
-    ) {
-      try {
-        if (!account) return;
-
-        const payload: InputTransactionData = {
-          data: {
-            function: `${moduleAddress}::${moduleName}::duel`,
-            functionArguments: [operation],
-          },
-        };
-        const response = await signAndSubmitTransaction(payload);
-        console.log(response);
-        const resultData = await client.getAccountResource({
-          accountAddress: account?.address,
-          resourceType: `${moduleAddress}::${moduleName}::DuelResult`,
-        });
-        console.log(resultData);
-      } catch (error) {
-        console.error(error);
-      } finally {
+  const handleTransaction = async (
+    payload: InputTransactionData,
+    move = ""
+  ) => {
+    if (!account) return;
+    setTransactionInProgress(true);
+    try {
+      const res = await signAndSubmitTransaction(payload);
+      console.log(res);
+      const resultData = await client.getAccountResource({
+        accountAddress: account?.address,
+        resourceType: `${moduleAddress}::${moduleName}::DuelResult`,
+      });
+      const gameResult = resultData.duel_result.toString();
+      if (gameResult === "Win") {
+        setResult("You won!");
+      } else if (gameResult === "Lose") {
+        setResult("You lost!");
+      } else {
+        setResult(gameResult);
       }
+      setComputerSelection(resultData.computer_selection.toString());
+    } catch (error) {
+      console.error("Transaction failed:", error);
+    } finally {
+      setTransactionInProgress(false);
     }
   };
 
@@ -141,7 +187,18 @@ function App() {
         <div className="absolute right-4 top-4 items-end">
           <WalletSelector />
         </div>
-        {connected ? GameWrapper2() : GameWrapper1()}
+        {connected ? (
+          <GameWrapper2
+            gameState={gameState}
+            onToggleGame={toggleGameState}
+            onMoveSelection={handleMoveSelection}
+            result={result}
+            computerSelection={computerSelection}
+            transactionInProgress={transactionInProgress}
+          />
+        ) : (
+          <GameWrapper1 />
+        )}
       </div>
     </>
   );
